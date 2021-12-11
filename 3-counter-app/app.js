bd2469a6c2d289df5d0f0d62aaaf22a7cc0bcce3
rdvnabay <ridvanabay@gmail.com>
2021-12-11 22:54:05 +0300

    counter-app

---
+++ 3-counter-app/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+            counter2: 0
+        }
+    },
+    methods: {},
+    computed: {
+        getCounterResult() {
+            console.log("Counter 1 çalıştı")
+            return this.counter > 5 ? "BÜYÜK" : "KÜÇÜK"
+        },
+        getCounter2Result() {
+            console.log("Counter 2 çalıştı")
+            return this.counter2 > 5 ? "BÜYÜK" : "KÜÇÜK"
+        }
+    },
+    watch: {
+        counter(newValue, oldValue) {
+            console.log("Counter", oldValue, "=>", newValue)
+        },
+        getCounterResult(newValue, oldValue) {
+            console.log("Result", oldValue, "=>", newValue)
+        }
+    }
+}).mount('#app')
