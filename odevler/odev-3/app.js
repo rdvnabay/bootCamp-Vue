6b42953b6d80f11691a7ce1a471962def2f7a697
rdvnabay <ridvanabay@gmail.com>
2021-12-12 16:06:27 +0300

    ödev 3

---
+++ odevler/odev-3/app.js
@@ -0,0 +1,28 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            value:0,
+            result2:null
+        }
+    },
+    computed:{
+        result(){
+            return this.value===33 ? "Tamamlandı":"Henüz tamamlanmadı"
+        }
+    },
+    watch:{
+        value(v){
+            if(v===33){
+                this.result2="Tamamlandı"
+            }
+            else{
+                this.result2="Henüz tamamlanmadı"
+            }
+        },
+        result(){
+            setTimeout(() => {
+                this.value=0
+            }, 2000);
+        }
+    }
+}).mount('#exercise')
